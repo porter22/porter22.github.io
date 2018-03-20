267b75406e20dc026ce3c8934d42e0620c271123
porter22 <idrissov22@gmail.com>
2018-03-20 18:19:28 +0100

    started week 4 exercise

---
+++ csvdateformatter.py
@@ -0,0 +1,30 @@
+import csv
+
+def time_to_minutes(mylist):
+    index = 0
+    minutes = 0
+    for item in mylist:
+        if index > 0:
+            print item[3]
+            timestr = str(item[3])
+            h,m,s = timestr.split(':')
+            minutes = int(h)*60 + int(m) + int(s)/60
+            print minutes
+            item[3] = minutes
+        index = index + 1
+
+    return mylist
+
+with open('marathon_times.csv', 'rb') as f:
+    reader = csv.reader(f)
+    mylist = list(reader)
+    print mylist
+    mylist = time_to_minutes(mylist)
+
+result = ""
+for item in mylist:
+    result = result + str(item[0]) + "," + str(item[1]) + "," + str(item[2]) + "," + str(item[3]) + "\n"
+print result
+
+with open("marathon_times_minutes.csv", "a") as text_file:
+        text_file.write(result)
