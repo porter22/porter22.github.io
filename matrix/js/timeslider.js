bbf3760535fef944a54c6a73bc5d9cd993a0b3eb
porter22 <idrissov22@gmail.com>
2018-09-28 14:52:56 +0200

    matrix update on click works

---
+++ matrix/js/timeslider.js
@@ -0,0 +1,141 @@
+function drawTimeslider() {
+
+  var margin = {top:50, right:50, bottom:0, left:50},
+        width = 820 - margin.left - margin.right,
+      height = 250 - margin.top - margin.bottom;
+
+  var histHeight = height;
+
+  var startDate = 1994,
+      endDate = 2019;
+
+  var barPadding = 1; // <-- New!
+
+  /*var dateArray = d3.timeYears(startDate, d3.timeYear.offset(endDate, 1));
+
+  var colours = d3.scaleOrdinal()
+      .domain(dateArray)
+      .range(['#ffc388','#ffb269','#ffa15e','#fd8f5b','#f97d5a','#f26c58','#e95b56','#e04b51','#d53a4b','#c92c42','#bb1d36','#ac0f29','#9c0418','#8b0000']);
+  */
+
+  d3.csv('timeslider_nonegatives.csv', function(error, data){
+
+    // x scale for time
+    var x = d3.scaleTime()
+        .domain([startDate, endDate])
+        .range([0, width])
+        .clamp(true);
+        //.ticks(d3.time.year,1);
+
+    // y scale for histogram
+    var y = d3.scaleLinear()
+        .range([0, histHeight-margin.top])
+        /*.domain([d3.min(data, function(d) {
+          //console.log(d.nodes);
+          return d.nodes; }), d3.max(data, function(d) {
+          //console.log(d.nodes);
+          return d.nodes; })]);*/
+         .domain(d3.extent(data, function(d) { return parseFloat(d.nodes); }));
+
+  //color scale
+    /*var c = d3.scaleLinear()
+        .range([0, 256])
+        .domain(d3.extent(data, function(d) { return parseFloat(d.nodes); }));*/
+
+        var c = d3.scaleOrdinal(d3.schemeCategory20c)
+                .domain(d3.extent(data, function(d) { return parseFloat(d.nodes); }));
+
+    var color_scale = d3.scaleLinear()
+                        .domain(d3.extent(data, function(d) { return parseFloat(d.nodes); }))
+                        .range(['beige', 'red']);
+
+    /*console.log("histHeight: ");
+    console.log(histHeight);
+    console.log(y(-7766));
+    console.log("scaleX: ");
+    console.log(x(2000));*/
+
+    var data3 = d3.range(0, 24).map(function (d) { return new Date(1995 + d, 10, 3); });
+    //var data3 = [1938, 1981, 1980, 0.015, 0.02, 0.025];;
+
+    /*var svg = d3.select("div#timeslider").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom);*/
+
+    var barchart = d3.select("div#timeslider").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom);
+
+    /*var slider  = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");*/
+
+      /*g.append("g")
+          .attr("class", "axis axis--x")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));*/
+
+      barchart.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+          .attr("class", function(d, i) {
+          return "bar" + d.year;
+          })
+          .attr("x", function(d, i) {
+          /*console.log(d.year);
+          console.log("x" + x(d.year));*/
+          return x(d.year);
+          })
+          .attr("y", function(d) {
+          return histHeight - y(d.nodes); //height minus data - to inverse bar chart
+          })
+          .attr("width", width / data.length)
+          .attr("height", function(d) {
+            /*console.log(d.year);
+            console.log(d.nodes);
+            console.log("y" + y(d.nodes));*/
+          return y(d.nodes);
+          })
+          .attr("fill", function(d) {
+          //console.log(parseFloat(c(d.nodes)));
+          return color_scale(d.nodes);
+          //return "rgb(0, 0, " + parseFloat(256 - c(d.nodes)) + ")"; //Math.round(parseFloat(c(d.nodes))*100)
+          });
+
+          barchart.selectAll("text")
+             .data(data)
+             .enter()
+             .append("text")
+             .text(function(d) {
+               /*console.log(d.year);
+               console.log("scaletext" + x(d.year));*/
+               return d.year;
+             })
+             .attr("x", function(d, i) {
+               return x(d.year) + width / data.length /2 ;
+             })
+             .attr("y", function(d) {
+               //return histHeight - y(d.nodes) - 15;
+               return histHeight + 15;
+             })
+             .attr("font-family", "sans-serif")
+             .attr("font-size", "11px")
+             .attr("fill", "black")
+             .attr("text-anchor", "middle");
+
+     /* var slider3 = d3.sliderHorizontal()
+              .min(d3.min(data3))
+              .max(d3.max(data3))
+              .step(1000 * 60 * 60 * 24 * 365)
+              .width(width + margin.left + margin.right)
+              .tickFormat(d3.timeFormat('%Y'))
+              .tickValues(data3)
+              .on('onchange', val => {
+                d3.select("p#value3").text(d3.timeFormat('%Y')(val));
+              });
+
+    slider.call(slider3);
+
+    d3.select("p#value3").text(d3.timeFormat('%Y')(slider3.value()));
+    d3.select("a#setValue3").on("click", () => slider3.value(new Date(1997, 11, 17))); */
+  })
+}//end function
---
+++ matrix/js/treemaps.js
@@ -0,0 +1,149 @@
+function drawTreemaps() {
+  var graphData = d3.json('treemap-all-years.json', function(error, data){
+  	// now you have your data
+  //  console.log(data.children);
+
+  	data.children.forEach(function(d,i) {
+
+  		if (d.name>1981 && d.name<2020) { //for demo purposes
+  			//dynamically create SVGs
+  		svgselection =	d3.select("div#treemaps")
+  			.append("svg")
+  			.attr("class", "cubes col-sm-2")
+  			.attr("height", "90px")
+  			.attr("id", function(dd) {
+  				//console.log(dd);
+  				return "svg" + i; });
+  		//TODO call draw function and pass svgid to it
+  		//draw("#svg1", "treemap.json");
+  			var arg1 = "#svg" + i;
+  		//	console.log(arg1);
+  			var arg2 = "treemap_" + i + ".json";
+  			//	console.log(arg2);
+
+  		//add years on top
+  		/*d3.select("div#treemaps")
+  		.append("text")
+  		.text(d.name)
+  			 .attr("x", 4)
+  			 .attr("y", 4)
+  			 .attr("font-family", "sans-serif")
+  			 .attr("font-size", "11px")
+  			 .attr("fill", "black")
+  			 .attr("text-anchor", "middle");*/
+
+  		draw(arg1,arg2,d);
+
+
+  		//console.log(svgs);
+  	}
+
+  	});//end for each children
+  });//end json
+
+  function draw(selector, url, datapoints){
+
+  //console.log(datapoints);
+  	var width = 75,
+  		height = 75;
+
+  	// var svg = d3.select("svg"),
+  	var svg = d3.select(selector)
+  			.append("svg")
+  			.attr("width", width)
+  			.attr("height", height)
+  			.on("click", function() {
+  					updateMatrix(datapoints.name);
+  			} );
+
+  	d3.select(selector).append('text')
+  			.text(datapoints.name)
+  			//.attr('class', 'legend')
+  			.attr("x", 17)
+  			.attr("y", -2)
+  			.attr("font-family", "sans-serif")
+  			.attr("font-size", "13px")
+  			.attr("fill", "black")
+  			.attr("font-weight", "600")
+  			.attr("text-anchor", "middle");
+
+   //TODO Rewrite color function, so that each color is fixed to a certain cluster
+
+  	 var color = d3.scaleOrdinal(d3.schemeCategory20c).domain([25,0]); //TODO reduce the number of clusters to <20
+
+  	var format = d3.format(",d");
+
+  	var treemap = d3.treemap()
+  			.tile(d3.treemapResquarify)
+  			.size([width, height])
+  			.round(true)
+  			.paddingInner(1);
+
+  	d3.json(url, function(error, data) {
+  		if (error) throw error;
+
+  		//console.log(data);
+
+  		var root = d3.hierarchy(data)
+  				.eachBefore(function(d) { d.data.id = (d.parent ? d.parent.data.id + "." : "") + d.data.name; })
+  				.sum(sumBySize)
+  				.sort(function(a, b) { return b.height - a.height || b.value - a.value; });
+
+  		treemap(root);
+
+  		var cell = svg.selectAll("g")
+  			.data(root.leaves())
+  			.enter().append("g")
+  				.attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
+
+  		cell.append("rect")
+  				.attr("id", function(d) { return d.data.id; })
+  				.attr("width", function(d) { return d.x1 - d.x0; })
+  				.attr("height", function(d) { return d.y1 - d.y0; })
+  				//.text(function(d) { return d.data.id + "\n" + format(d.value); })
+  				//.text(function(d) { return "test" })
+  				.attr("fill", function(d) {
+  					//extract clusterID from the treemapID
+  					var tempstr = d.parent.data.id.toString();
+  					var separate = tempstr.split(".");
+  					/*console.log(separate[2]);//this is a clusterID
+  					console.log(color(separate[2]));//this is a clusterID*/
+  					return color(separate[2]); });
+
+
+  		cell.append("title")
+  				.text(function(d) { return d.data.id + "\n" + format(d.value); });
+
+  		d3.selectAll("input")
+  				.data([sumBySize, sumByCount], function(d) { return d ? d.name : this.value; })
+  				.on("change", changed);
+
+  		var timeout = d3.timeout(function() {
+  			d3.select("input[value=\"sumByCount\"]")
+  					.property("checked", true)
+  					.dispatch("change");
+  		}, 2000);
+
+  		function changed(sum) {
+  			timeout.stop();
+
+  			treemap(root.sum(sum));
+
+  			cell.transition()
+  					.duration(750)
+  					.attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; })
+  				.select("rect")
+  					.attr("width", function(d) { return d.x1 - d.x0; })
+  					.attr("height", function(d) { return d.y1 - d.y0; });
+  		}
+  	});
+  }
+
+  function sumByCount(d) {
+  	return d.children ? 0 : 1;
+  }
+
+  function sumBySize(d) {
+  	return d.size;
+  }
+}
